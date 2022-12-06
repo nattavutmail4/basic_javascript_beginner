3d97903431adb52a391b976b7b8cbeaa0b8c08e3
AdminOffice <nattavutmail3@gmail.com>
2022-12-06 13:23:29 +0700

    ep6 เรียนรู้การทำ Loop

---
+++ Basic/6-Loop-for-while-break-continue/main.js
@@ -0,0 +1,45 @@
+let id = 1;
+// ข้อมูลชนิด array in object
+// const person = []
+
+// const fetchUsers = async ()=>{
+//   const resp = await (await fetch('https://rickandmortyapi.com/api/character')).json()
+//   let  count = resp.results.length
+//   let  data  = resp.results
+//   for(let i = 0; i<=count; i++){
+//     if(data[i]!=undefined){
+//        console.log(data[i])
+//     }
+//   }
+// }
+// fetchUsers()
+
+// const person = {
+//     firstName:"John",
+//     lastName:"Doe",
+//     age:26
+// }
+// for(const key in person){
+//     console.log(person[key])
+// }
+
+// const cars = ['bmw','toyota','mini','volvo']
+
+// for(const car of cars){
+//     console.log(car)
+// }
+
+// const vaccines = ['sinovac','pfizer','moderna']
+// for(const vaccine of vaccines){
+//     console.log(vaccine)
+// }
+
+for(let index =0 ; index<= 10 ; index++)
+{
+    if(index ==5){
+        console.log('break' ,index);
+        break
+    }else{
+        
+    }
+}
